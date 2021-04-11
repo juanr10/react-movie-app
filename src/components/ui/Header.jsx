6a7c77a8c3ab7762e409b2feb85acaebdbc96fe6
juanr10 <argudojuan7@gmail.com>
2021-04-11 15:15:51 +0200

    styles from Header component updated

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
                 <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-                <span className="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+                <span className="font-semibold text-xl tracking-tight">iMovie</span>
             </div>
             <div className="block lg:hidden">
                 <button 
@@ -19,18 +19,18 @@ const Header = () => {
             </div>
             <div className={"w-full block flex-grow lg:flex lg:items-center lg:w-auto transition duration-500 ease-in-out " + (navbarOpen ? '' : 'hidden')}>
                 <div className="text-md lg:flex-grow text-white">
-                    <a href="/" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-purple-300 mr-4 ">
-                        Docs
+                    <a href="/" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-purple-300 mx-8">
+                        Populares
                     </a>
-                    <a href="/" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-purple-300 mr-4">
-                        Examples
+                    <a href="/" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-purple-300 mx-8">
+                        Próximamente
                     </a>
-                    <a href="/" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-purple-300">
-                        Blog
+                    <a href="/" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-purple-300 mx-8">
+                        Más votadas
                     </a>
                 </div>
                 <div>
-                    <a href="/" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-purple-200 hover:text-white hover:bg-purple-400 mt-4 lg:mt-0">Download</a>
+                    <a href="/" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-purple-200 hover:text-white hover:bg-purple-400 mt-4 mx-8 lg:mt-0">Download</a>
                 </div>
             </div>
          </nav>
