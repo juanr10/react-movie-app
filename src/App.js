8fd29c0d3d8dfd23ece12ea944135ed6d75f9813
juanr10 <argudojuan7@gmail.com>
2021-04-11 14:28:25 +0200

    tailwind installed & configurated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
+// Styles
+import './tailwind.output.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
+    <div className="min-h-screen bg-gray-900">
+      <h2 className="text-center text-4xl font-bold text-white">TailwindCSS + React</h2>
     </div>
-  );
+  )
 }
 
 export default App;
